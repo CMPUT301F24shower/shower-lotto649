ed0228d44ce104df1d51fa1d858d7a1f9a7c55f3
Isaac Vissers <ivissers@uaberta.ca>
2024-11-03 11:19:54 -0700

    Task bar now works for admin users, there are still issues for admin/users as the task bar would be too large

--- app/src/main/java/com/example/lotto649/MainActivity.java
+++ app/src/main/java/com/example/lotto649/MainActivity.java
@@ -9,15 +9,32 @@
 package com.example.lotto649;
 
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.lotto649.Models.FirestoreIsAdminCallback;
+import com.example.lotto649.Models.FirestoreUserCallback;
+import com.example.lotto649.Models.UserModel;
 import com.example.lotto649.Views.Fragments.AccountFragment;
+import com.example.lotto649.Views.Fragments.BrowseEventsFragment;
+import com.example.lotto649.Views.Fragments.BrowseFacilitiesFragment;
+import com.example.lotto649.Views.Fragments.BrowseProfilesFragment;
 import com.example.lotto649.Views.Fragments.CameraFragment;
 import com.example.lotto649.Views.Fragments.FacilityFragment;
 import com.example.lotto649.Views.Fragments.HomeFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity
         implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -39,12 +56,25 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.getMenu().clear();
 
         // Set the listener for navigation item selection
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
 
-        // Set the default selected item to "account"
-        bottomNavigationView.setSelectedItemId(R.id.account);
+        checkUserAdminStatus(new FirestoreIsAdminCallback() {
+            @Override
+            public void onCallback(boolean isAdmin) {
+                if (isAdmin) {
+                    bottomNavigationView.inflateMenu(R.menu.bottom_nav_menu_admin);
+                    // Set the default selected item to "browseProfiles"
+                    bottomNavigationView.setSelectedItemId(R.id.browseProfiles);
+                } else {
+                    bottomNavigationView.inflateMenu(R.menu.bottom_nav_menu);
+                    // Set the default selected item to "home"
+                    bottomNavigationView.setSelectedItemId(R.id.home);
+                }
+            }
+        });
     }
 
     // Create fragment instances
@@ -52,6 +82,9 @@ public class MainActivity extends AppCompatActivity
     CameraFragment cameraFragment = new CameraFragment();
     AccountFragment accountFragment = new AccountFragment();
     FacilityFragment facilityFragment = new FacilityFragment();
+    BrowseEventsFragment browseEventsFragment = new BrowseEventsFragment();
+    BrowseProfilesFragment browseProfilesFragment = new BrowseProfilesFragment();
+    BrowseFacilitiesFragment browseFacilitiesFragment = new BrowseFacilitiesFragment();
 
     /**
      * Handles the selection of items from the BottomNavigationView.
@@ -87,8 +120,55 @@ public class MainActivity extends AppCompatActivity
                     .replace(R.id.flFragment, facilityFragment)
                     .commit();
             return true;
+        } else if (item.getItemId() == R.id.browseEvents) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, browseEventsFragment)
+                    .commit();
+            return true;
+        } else if (item.getItemId() == R.id.browseFacilities) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, browseFacilitiesFragment)
+                    .commit();
+            return true;
+        } else if (item.getItemId() == R.id.browseProfiles) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.flFragment, browseProfilesFragment)
+                    .commit();
+            return true;
         } else {
             return false;
         }
     }
+
+
+
+    private void checkUserAdminStatus(FirestoreIsAdminCallback firestoreIsAdminCallback) {
+        String deviceId = Settings.Secure.getString(this.getContentResolver(), Settings.Secure.ANDROID_ID);
+        DocumentReference doc = FirebaseFirestore.getInstance().collection("users").document(deviceId);
+
+        doc.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Boolean isAdmin = document.getBoolean("admin");
+                        if (isAdmin != null && isAdmin) {
+                            firestoreIsAdminCallback.onCallback(true);
+                        } else {
+                            firestoreIsAdminCallback.onCallback(false);
+                        }
+                    } else {
+                        firestoreIsAdminCallback.onCallback(false);
+                    }
+                } else {
+                    firestoreIsAdminCallback.onCallback(false);
+                }
+            }
+        });
+    }
+
 }
