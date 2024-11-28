77302ed1b7ae63750584f5ffd0643a700d284e07
Gauravdeep Bachhal <gbachhal@ualberta.ca>
2024-11-28 13:29:08 -0700

    notifications basic functionality works

---
+++ app/src/main/java/com/example/lotto649/NotificationHelper.java
@@ -0,0 +1,50 @@
+package com.example.lotto649;
+
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+public class NotificationHelper {
+
+    public void sendNotification(Context context, CharSequence textTitle, String textContent) {
+        String CHANNEL_ID = "test_channel";
+        int NOTIFICATION_ID = 1;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(R.drawable.calendar_icon_foreground)
+                .setContentTitle(textTitle)
+                .setContentText(textContent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, textTitle, importance);
+            channel.setDescription(textContent);
+
+            NotificationManager notificationManager = getSystemService(context, NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+
+    }
+}
