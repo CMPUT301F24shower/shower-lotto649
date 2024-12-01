b5ec2414abbd4bd9b4688b14d3f6c713ae8c9f9a
Gauravdeep Bachhal <gbachhal@ualberta.ca>
2024-11-30 19:59:03 -0700

    first try at opening camera in the app

--- app/src/main/java/com/example/lotto649/Views/Fragments/CameraFragment.java
+++ app/src/main/java/com/example/lotto649/Views/Fragments/CameraFragment.java
@@ -10,18 +10,41 @@
  * </p>
  */
 package com.example.lotto649.Views.Fragments;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.camera.core.Camera;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+
 import com.example.lotto649.R;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+
 // TODO this class can likely be deleted, along with the entire page
 public class CameraFragment extends Fragment {
+
+    private static final int REQUEST_CODE_PERMISSIONS = 10;
+    private static final String[] REQUIRED_PERMISSIONS = {Manifest.permission.CAMERA};
+    private PreviewView previewView;
+
     /**
      * Public empty constructor for CameraFragment.
      * <p>
-     * Required for proper instantiation of the fragment by the Android system.
+     * Required for prope   r instantiation of the fragment by the Android system.
      * </p>
      */
     public CameraFragment() {
@@ -38,7 +61,59 @@ public class CameraFragment extends Fragment {
      */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        previewView = view.findViewById(R.id.previewView);
+
+        if (allPermissionsGranted()) {
+            startCamera();
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+        }
+
+        return view;
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(requireContext(), permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void startCamera() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(requireContext());
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                PreviewView previewView = requireView().findViewById(R.id.previewView);
+
+
+                Preview preview = new Preview.Builder()
+                        .setTargetRotation(Surface.ROTATION_0)
+                        .build();
+
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+                Camera camera = cameraProvider.bindToLifecycle(this, androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA, preview);
+
+            } catch (ExecutionException | InterruptedException e) {
+                Log.e("CameraFragment", "Use case binding failed", e);
+            }
+        }, ContextCompat.getMainExecutor(requireContext()));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera();
+            } else {
+                Log.e("CameraFragment", "Permissions not granted by the user.");
+            }
+        }
     }
 }
