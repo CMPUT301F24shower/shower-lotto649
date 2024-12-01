e12a80838c8aba274cf80638bbacd8b9dccdc00a
Gauravdeep Bachhal <gbachhal@ualberta.ca>
2024-12-01 10:20:50 -0700

    camera should scan for barcodes now

--- app/src/main/java/com/example/lotto649/Views/Fragments/CameraFragment.java
+++ app/src/main/java/com/example/lotto649/Views/Fragments/CameraFragment.java
@@ -21,7 +21,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.camera.core.Camera;
+import androidx.annotation.OptIn;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ExperimentalGetImage;
+import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
@@ -31,12 +34,15 @@ import androidx.fragment.app.Fragment;
 
 import com.example.lotto649.R;
 import com.google.common.util.concurrent.ListenableFuture;
-
-import java.util.concurrent.ExecutionException;
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
 
 // TODO this class can likely be deleted, along with the entire page
 public class CameraFragment extends Fragment {
 
+    private static final String TAG = "CameraFragment";
     private static final int REQUEST_CODE_PERMISSIONS = 10;
     private static final String[] REQUIRED_PERMISSIONS = {Manifest.permission.CAMERA};
     private PreviewView previewView;
@@ -82,6 +88,7 @@ public class CameraFragment extends Fragment {
         return true;
     }
 
+    @OptIn(markerClass = ExperimentalGetImage.class)
     private void startCamera() {
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
                 ProcessCameraProvider.getInstance(requireContext());
@@ -96,12 +103,35 @@ public class CameraFragment extends Fragment {
                         .setTargetRotation(Surface.ROTATION_0)
                         .build();
 
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
+
+                ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+                imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(requireContext()), image -> {
+                    InputImage inputImage = InputImage.fromMediaImage(image.getImage(), image.getImageInfo().getRotationDegrees());
+                    BarcodeScanner scanner = BarcodeScanning.getClient();
+
+                    scanner.process(inputImage)
+                            .addOnSuccessListener(barcodes -> {
+                                for (Barcode barcode : barcodes) {
+                                    String rawValue = barcode.getRawValue();
+                                    Log.d(TAG, "QR Code detected: " + rawValue);
+                                    // Optionally, navigate or pass data here
+                                }
+                            })
+                            .addOnFailureListener(e -> Log.e(TAG, "Failed to scan QR code", e))
+                            .addOnCompleteListener(task -> image.close());
+                });
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalysis);
                 preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-                Camera camera = cameraProvider.bindToLifecycle(this, androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA, preview);
-
-            } catch (ExecutionException | InterruptedException e) {
-                Log.e("CameraFragment", "Use case binding failed", e);
+            } catch (Exception e) {
+                    Log.e(TAG, "Camera initialization failed", e);
             }
         }, ContextCompat.getMainExecutor(requireContext()));
     }
